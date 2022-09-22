25ddb5dce6ac04342c0a958c3008382d89e97ef4
abadr2020 <ahmed.badr@suezcanal.gov.eg>
2022-09-22 21:19:23 +0200

    change front end deploy script

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
 aws s3 cp --recursive --acl public-read ./www s3://udagram-frontend-badr/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagram-frontend-badr/
+aws s3 website s3://udagram-frontend-badr/ --index-document index.htm --error-document index.html
